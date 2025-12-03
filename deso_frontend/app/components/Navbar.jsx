fb850b7de821a7b866d976b3fa7589bd004b5362
maxim <maxim@PC>
2025-12-03 16:32:50 -0300

    Merge origin/master
    # Conflicts:
    #	nbproject/project.properties
    #	src/LogicaNegocio/Gestores/GestorHabitacionYReserva.java
    #	src/Ventanas/MostrarEstadoHabitacionPanelCU15.java
    #	src/Ventanas/OcuparHabitacionPanel_1.form
    #	src/Ventanas/OcuparHabitacionPanel_1.java
    #	src/Ventanas/ReservarHabitacionPanel_1.form
    #	src/Ventanas/ReservarHabitacionPanel_1.java
    #	src/Ventanas/ReservarHabitacionPanel_2.form
    #	src/Ventanas/ReservarHabitacionPanel_2.java
    #	src/Ventanas/ReservarHabitacionPanel_3.form
    #	src/Ventanas/ReservarHabitacionPanel_3.java
    #	src/Ventanas/Utils/VentanaUtils.java

--- deso_frontend/app/components/Navbar.jsx
+++ deso_frontend/app/components/Navbar.jsx
@@ -1,17 +1,14 @@
-"use client";
-import "../globals.css";
-
 export default function Navbar(){
     return (
         <div className="navbar">
             <button className="icon_btn">
-                <img src="/menu.png" alt="menu" width={32} height={32}/>
+                <img src="/menu.png" alt="menu" width={38} height={38}/>
             </button>
             <div className="navbar_center">
                 <img src="/logo.png" alt="logo" width={110} height={110}/>
             </div>
             <button className="icon_btn">
-                <img src="/settings.png" alt="settings" width={32} height={32}/>
+                <img src="/settings.png" alt="settings" width={38} height={38}/>
             </button>
         </div>
     )
--- deso_frontend/app/page.tsx
+++ deso_frontend/app/page.tsx
@@ -2,47 +2,52 @@
 
 import "./globals.css";
 import Link from "next/link";
-import Navbar from "./components/Navbar";
-
-import { useState } from "react";
 
 export default function MenuPrincipal() {
-
   return (
     <main className="fondo">
-      
 
       <h1 className="titulo">MENU PRINCIPAL</h1>
       <h3 className="subtitulo">Elija una de las siguientes opciones</h3>
+
       <div className="contenedor">
+
         <Link href="/mostrarEstadoHabitaciones">
-        <button className="boton">
-          Mostar estado Habitaciones
-        </button>
+          <button className="boton">
+            <img src="/lupa.png" className="iconos_boton" />
+             <h3 className="letra_btn">Mostrar Estado Habitaciones</h3>
+          </button>
         </Link>
+
         <Link href="/darDeBajaHuesped">
-        <button className="boton">
-          Dar de Baja Huesped
-        </button>
+          <button className="boton">
+            <img src="/darbaja.png" className="iconos_boton" />
+             <h3 className="letra_btn">Dar de Baja Huesped</h3>
+          </button>
         </Link>
+
         <Link href="/reservarHabitacion">
-        <button className="boton">
-          Reservar Habitacion
-        </button>
+          <button className="boton">
+            <img src="/calendario.png" className="iconos_boton" />
+             <h3 className="letra_btn">Reservar Habitacion</h3>
+          </button>
         </Link>
+
         <Link href="/cancelarReserva">
-        <button className="boton">
-          Cancelar Reserva
-        </button>
+          <button className="boton">
+            <img src="/cancelar.png" className="iconos_boton" />
+             <h3 className="letra_btn">Cancelar Reserva</h3>
+          </button>
         </Link>
+
         <Link href="/facturar">
-        <button className="boton">
-          Facturar
-        </button>
+          <button className="boton">
+            <img src="factura.png" className="iconos_boton" />
+             <h3 className="letra_btn">Facturar</h3>
+          </button>
         </Link>
+
       </div>
-
-
     </main>
   );
 }
