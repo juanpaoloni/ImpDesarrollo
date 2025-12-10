b98b63579a174f3b1e91df00f12bd139609ec7bf
juanpaoloni <juanignaciopaoloni@gmail.com>
2025-12-10 20:03:40 -0300

    Bdd completa

--- src/main/java/com/DESO_TP/DESO_backend/DataAccessObject/ReservaDAO.java
+++ src/main/java/com/DESO_TP/DESO_backend/DataAccessObject/ReservaDAO.java
@@ -5,8 +5,12 @@
 package com.DESO_TP.DESO_backend.DataAccessObject;
 
 import com.DESO_TP.EntidadesDominio.Reserva;
+import com.DESO_TP.Enumerados.EstadoReserva;
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -18,5 +22,9 @@ public interface ReservaDAO extends CrudRepository<Reserva, Long>{
     List<Reserva> findByHabitacion_NumeroHabitacion(Integer numeroHabitacion);
     
     List<Reserva> findByApellidoAndNombre(String apellido, String nombre);
+    
+    @Modifying
+    @Query("UPDATE Reserva r SET r.estadoReserva = :estado WHERE r.id = :id")
+    void actualizarEstado(@Param("id") Long id, @Param("estado") EstadoReserva estado);
 }
 
